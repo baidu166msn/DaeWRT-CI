b1dc5470ed17571708d5f07de599bf10a065dd6c
davidtall <269700927@qq.com>
2026-07-21 09:45:09 +0800

    增加开关是否使用最新的Makefile 编译

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -140,6 +140,13 @@ rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypa
 rm -rf ../feeds/packages/net/{v2ray-geodata,dae*}
 
 cp -r $GITHUB_WORKSPACE/package/v2ray-geodata ./
+
+if [[ "${USE_QIUSIMONS_DAE_MAKEFILE,,}" == "true" ]]; then
+	git clone --depth=1 --single-branch --branch kix "https://github.com/QiuSimons/luci-app-dae.git" luci-app-dae-QiuSimons
+	rm -rf luci-app-dae/dae/Makefile && cp -r luci-app-dae-QiuSimons/dae/Makefile luci-app-dae/dae/
+	cat luci-app-dae/dae/Makefile
+fi
+
 #修复daed/Makefile
 #rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
 sed -i 's/pnpm install ; \\/pnpm install --no-frozen-lockfile ; \\/g' luci-app-daed/daed/Makefile
